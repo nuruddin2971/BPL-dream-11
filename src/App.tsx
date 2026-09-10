00106b15c1f9896f8e2d3d79d115b1a3e696943c
nuruddin2971 <nuruddin2971@gmail.com>
2026-09-10 21:15:22 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h2 className="text-5xl text-red-500">BPL Dream 11</h2>
+      <button className="btn btn-error">Error</button>
+    </>
+  );
+}
+
+export default App;
