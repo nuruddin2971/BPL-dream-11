436294915890bfb12b2d46c704d88e3860da1546
nuruddin2971 <nuruddin2971@gmail.com>
2026-09-12 22:35:21 +0600

    last commit

--- src/components/players/SelectedPlayerCard.tsx
+++ src/components/players/SelectedPlayerCard.tsx
@@ -1,4 +1,4 @@
-import React, { type Dispatch, type SetStateAction } from "react";
+import { type Dispatch, type SetStateAction } from "react";
 import type { Iplayer } from "../../types/player";
 import { TbTrash } from "react-icons/tb";
 
--- src/components/players/SelectedPlayers.tsx
+++ src/components/players/SelectedPlayers.tsx
@@ -1,4 +1,4 @@
-import React, { useState, type Dispatch, type SetStateAction } from "react";
+import { type Dispatch, type SetStateAction } from "react";
 import type { Iplayer } from "../../types/player";
 import SelectedPlayerCard from "./SelectedPlayerCard";
 
